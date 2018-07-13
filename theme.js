9a92181430b3ea190ed0c8afe82d48a426b6d081
meiyulu <meiyulu@yqjr.com.cn>
2018-07-13 14:37:12 +0800

    删除多余的文件

--- theme.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  'brand-primary': '#fff445', //主题色
-};
--- utils/count.js
+++
@@ -1,7 +0,0 @@
-export default function count(id) {
-  try {
-    window._hmt.push(['_trackEvent', id, 'click']);
-  } catch (error) {
-    console.log(error);
-  }
-}
